8a7f353f5d32fa121f52c3a19a179505886d1a70
tom <tomcmead0@gmail.com>
2022-09-28 15:20:39 +0100

    feat: Scrape rightmove for properties with region code provided

--- property-api/property_api.py
+++ property-api/property_api.py
@@ -5,7 +5,8 @@ import resources.rightmove
 app = flask.Flask(__name__)
 api = flask_restful.Api(app)
 
-api.add_resource(resources.rightmove.Location, '/rightmove')
+api.add_resource(resources.rightmove.Location, '/location')
+api.add_resource(resources.rightmove.RightmoveScraper, '/rightmove')
 
 if __name__ == '__main__':
     app.run(debug=True)
