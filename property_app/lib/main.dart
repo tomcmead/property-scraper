e2888dcade2eac1aed409da4961440775e408c7b
tom <tomcmead0@gmail.com>
2022-10-27 15:40:28 +0100

    feat: Added initial app bar

--- property_app/lib/main.dart
+++ property_app/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Hello, world'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("Property Hunter"),
+      centerTitle: true,
+      backgroundColor: Color.fromARGB(255, 38,130,166),
+    ),
+    )
 ));
