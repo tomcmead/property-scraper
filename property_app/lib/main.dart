46d65a1940ea77ea29a07871f78fe68123865037
tom <tomcmead0@gmail.com>
2022-11-01 18:44:16 +0000

    feat: Add top tab bar with empty home and settings pages

--- property_app/lib/main.dart
+++ property_app/lib/main.dart
@@ -1,21 +1,41 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
   MaterialApp(
-    home: Home(),
+    home: MainPage(),
   )
 );
 
 
-class Home extends StatelessWidget {
+class MainPage extends StatefulWidget {
   @override
-  Widget build (BuildContext context){
-    return Scaffold(
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage>{
+  @override
+  Widget build (BuildContext context) => DefaultTabController(
+    length: 2, 
+    child: Scaffold(
       appBar: AppBar(
         title: Text("Property Hunter"),
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 38,130,166),
+        bottom: TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.home)),
+            Tab(icon: Icon(Icons.settings))
+          ],
+        ),
       ),
-    );
-  }
+      body: TabBarView(
+        children: [
+          Center(child: Text('Home')),
+          Center(child: Text('Settings')),
+        ],
+      ),
+    ),
+  );
 }
